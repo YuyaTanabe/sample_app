0198bab9ff3358634714bd38b2dde11e999b7251
YuyaTanabe <gkumanokawa@gmail.comgit config --global user.email gkumanokawa@gmail.comgit config --global user.email gkumanokawa@gmail.com>
2018-11-10 03:36:32 +0000

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e55cb15890cf933246f0bd60f37b1f9ad32af5d4e46b04d3980702ee0621db3fcab22e1e531f59d20a33f902a6090a30336592ee9dc9a8fcc192c2f817ace3a2'
